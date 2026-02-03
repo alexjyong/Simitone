9e85215e468b71ec9e8935e9bf5cf60067eb298d
Alex Yong <alexjyong@gmail.com>
2026-02-03 07:10:20 -0500

    adding in update checker

---
+++ Client/Simitone/Simitone.Client/SimitoneVersion.cs
@@ -0,0 +1,25 @@
+/*
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one at
+http://mozilla.org/MPL/2.0/.
+*/
+
+namespace Simitone.Client
+{
+    /// <summary>
+    /// Version constants for Simitone.
+    /// Update the Current version for each release.
+    /// </summary>
+    public static class SimitoneVersion
+    {
+        /// <summary>
+        /// The current version of Simitone. Update this for each release.
+        /// </summary>
+        public const string Current = "0.8.19-forked";
+
+        /// <summary>
+        /// GitHub API endpoint for fetching the latest release.
+        /// </summary>
+        public const string GitHubReleasesApiUrl = "https://api.github.com/repos/alexjyong/Simitone/releases/latest";
+    }
+}
